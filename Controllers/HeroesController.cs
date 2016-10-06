f347cc92cd15a7278d3cb606d83c59bae103bf78
Shayne Boyer <spboyer@live.com>
2016-10-06 15:24:43 -0400

    usings clean up

--- Controllers/HeroesController.cs
+++ Controllers/HeroesController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GenFu;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
--- Controllers/LegionsController.cs
+++ Controllers/LegionsController.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using GenFu;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 

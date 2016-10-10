bd417477b96d7802ba0dd76ac4df624771eba9cf
Shayne Boyer <spboyer@live.com>
2016-10-10 13:17:50 -0400

    cleanup comment

--- Controllers/HeroesController.cs
+++ Controllers/HeroesController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using GenFu;
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace superhero
 {

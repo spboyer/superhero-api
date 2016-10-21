193777e30559085b07c230b793831c21921912a7
Shayne Boyer <spboyer@live.com>
2016-10-20 22:55:40 -0400

    fix undefined using elvis

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1><my-legion></my-legion>'
+    template: '<h1>Angular Legion Generator</h1><my-legion></my-legion>'
 })
 export class AppComponent { }
